a0e7fd16ba46517e09591381d31ab805552c6d0a
Thomas Blion <tblion@users.noreply.github.com>
2021-07-13 08:46:49 +0200

    Add api server poc

---
+++ server/index.js
@@ -0,0 +1,42 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/testApi',(req,res)=>{
+    res.status(200).send(callName("test.jpg", res))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+function callName(req, res) { 
+      
+    // Use child_process.spawn method from  
+    // child_process module and assign it 
+    // to variable spawn 
+    var spawn = require("child_process").spawn; 
+      
+    // Parameters passed in spawn - 
+    // 1. type_of_script 
+    // 2. list containing Path of the script 
+    //    and arguments for the script  
+    console.log(req)
+    // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
+    // so, first name = Mike and last name = Will 
+    var process = spawn('python',["./test.py", 
+                            req] ); 
+  
+    // Takes stdout data from script which executed 
+    // with arguments and send this data to res object 
+    process.stdout.on('data', function(data) { 
+        console.log(data.toString());
+        res.send(data.toString()); 
+    } ) 
+} 
+
---
+++ server/test.py
@@ -0,0 +1,5 @@
+import cv2
+import matplotlib.pyplot as plt
+image = cv2.imread("test.jpg",0)
+plt.imshow(image)
+plt.show()
