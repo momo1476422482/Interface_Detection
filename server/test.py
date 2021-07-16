7e8ac97e49b12af106a09cfe5ab0d98abfbbd7fd
elmahdimeghras <86477388+elmahdimeghras@users.noreply.github.com>
2021-07-16 10:05:32 +0200

    Update test.py

--- server/test.py
+++ server/test.py
@@ -1,5 +1,6 @@
 import cv2
 import matplotlib.pyplot as plt
-image = cv2.imread("test.jpg",0)
+image = cv2.imread("test.jpg",1)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.imshow(image)
 plt.show()
